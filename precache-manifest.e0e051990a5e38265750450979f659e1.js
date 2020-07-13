a0741945fb735f89cbc2aa964eec987074596ef9
Philip Ridgill <philipridgill@gmail.com>
2020-07-13 14:22:22 +0100

    Updates

---
+++ precache-manifest.e0e051990a5e38265750450979f659e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a69fab4faf555dda219c71d2f2e1773",
+    "url": "/udemy-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f9fe0ca08599202e915f",
+    "url": "/udemy-monsters-rolodex/static/css/main.5a821bbb.chunk.css"
+  },
+  {
+    "revision": "ef9e515cc7cf07574d6e",
+    "url": "/udemy-monsters-rolodex/static/js/2.bef12f9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy-monsters-rolodex/static/js/2.bef12f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9fe0ca08599202e915f",
+    "url": "/udemy-monsters-rolodex/static/js/main.e4ac58ab.chunk.js"
+  },
+  {
+    "revision": "16a1d39489b9e2a4fb3c",
+    "url": "/udemy-monsters-rolodex/static/js/runtime-main.a1b71a5f.js"
+  }
+]);
